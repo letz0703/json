e4c110cebbc49954aaee247194171b7abf30faf4
ManI <letz0703@gmail.com>
2023-09-16 03:15:43 +0900

    trim

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <div>App</div>
+  return (
+    <>
+      <h1>jsons from ESP List</h1>
+      <ul>
+        <li>
+          <a href="https://www.especialist.org/json/db.json">db.json</a>
+        </li>
+      </ul>
+    </>
+  )
 }
 
 export default App
